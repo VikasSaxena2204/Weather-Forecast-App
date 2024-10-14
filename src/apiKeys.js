edd1c418af12b951a5adfc85da9415a5ee7f9cd2
VIKAS_ <78485682+VikasSaxena2204@users.noreply.github.com>
2024-10-14 14:38:18 +0530

    apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,6 +1,6 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-    key: "9ad9ae4068557a81563e62097365dd94",
+    key: "{Your Weather API Keys}",
     base: "https://api.openweathermap.org/data/2.5/",
   };
   
